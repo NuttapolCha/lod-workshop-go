f53abda4a7e62a9452f86c4fe9427f1b5ad2e1a2
nuttapol <natthapol.ietz@gmail.com>
2023-09-15 10:01:02 +0700

    ✨ introduce with promotion problem

---
+++ workshop/promotion.go
@@ -0,0 +1,10 @@
+package workshop
+
+type Promotion struct {
+	Discount int
+}
+
+func HandlePersonPurchaseProductsWithPromotion(person *Person, shop *Shop, promotion Promotion, productName string, quantity int) bool {
+
+	return false
+}
