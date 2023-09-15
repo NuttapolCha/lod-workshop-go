983367da6c44e8b34e33f6ff053c7ea64ec7efc0
nuttapol <natthapol.ietz@gmail.com>
2023-09-15 10:02:20 +0700

    ✨ poor solution with promotion

--- workshop/promotion.go
+++ workshop/promotion.go
@@ -5,6 +5,16 @@ type Promotion struct {
 }
 
 func HandlePersonPurchaseProductsWithPromotion(person *Person, shop *Shop, promotion Promotion, productName string, quantity int) bool {
-
+	for _, shelf := range shop.Shelves {
+		for i, product := range shelf.Products {
+			if product.Name == productName {
+				if person.Wallet.Cash.Amount >= product.Price.Amount*quantity-promotion.Discount && product.Quantity >= quantity {
+					person.Wallet.Cash.Amount -= product.Price.Amount*quantity - promotion.Discount
+					shelf.Products[i].Quantity -= quantity
+					return true
+				}
+			}
+		}
+	}
 	return false
 }
